2d74ff3eb34e9c6515d4e1e1f9119e88ffa6db5c
Luiz Thomasini <luizthomasini@gmail.com>
2022-04-04 23:51:18 -0300

    Lista 01

--- Listas/Lista_01/README.md
+++ Listas/Lista_01/README.md
@@ -1,6 +1,7 @@
 ## Lista de Exercícios 1 – Processamento Gráfico
 ### Introdução à OpenGL Moderna – Shaders & Buffers
 
+---
 1 - O que é a GLSL? Quais os dois tipos de shaders são obrigatórios no pipeline programável 
 da versão atual que trabalhamos em aula e o que eles processam?
 
@@ -9,3 +10,39 @@ GLSL (OpenGL Shading Language) é uma linguagem de programação imperativa base
 sem ter que utilizar linguagens de mais baixo nível, como assembly.
 
 Os principais shaders utilizados no pipeline são o Vertex shader e o Fragment shader.
+
+- Vertex shader - processa os vértices individuais, se preocupa com dados posicionais
+- Fragment shader - processa os pixels, preocupa-se com o cálculo do output de cor dos pixels
+
+---
+2 - O que são primitivas gráficas? Como fazemos o armazenamento dos vértices na OpenGL?
+
+Primitivas gráficas são "dicas" que passamos ao OpenGL sobre a forma como queremos renderizar os dados.
+Ex. pontilhado? uma única linha? uma coleção de triângulos?
+
+Armazenamos dados dos vértices direto na memória da GPU. Para isso, utilizamos VBO (vertex buffer objects), 
+um objeto buffer que abstrai e facilita o gerenciamento dessa memória.
+
+---
+3 - Explique o que é VBO, VAO e EBO, e como se relacionam.
+
+São estruturas que hajem como handlers da memória da GPU. Como a GPU só trabalha com dados que estão na
+sua memória, precisamos dessas estruturas para poder manipular.
+
+VBO (vertex buffer objects) - armazena os dados dos vértices
+VAO (vertex array objects) - handle para VBOs, um VAO pode armazenar um ou mais VBOs
+EBO (element buffer objects) - buffer que, também armazena dados dos vértices, mas permite indexar os valores de modo que seja mais simples de manipular multiplas formas (o início de um é o começo de outro)
+
+---
+5 - Faça o desenho de 2 triângulos na tela. Desenhe eles:
+
+a. Apenas com o polígono preenchido
+![](https://i.imgur.com/fArcvZk.png)
+
+b. Apenas com contorno
+![](https://i.imgur.com/cYNI7EO.png)
+
+c. Apenas como pontos
+![](https://i.imgur.com/4ZIx2Q5.png)
+
+d. Com as 3 formas de desenho juntas
--- Listas/Lista_01/src/Source.cpp
+++ Listas/Lista_01/src/Source.cpp
@@ -1,18 +1,7 @@
-#include <iostream>
-
-// GLAD
 #include <glad/glad.h>
-
-// GLFW
 #include <GLFW/glfw3.h>
 
-
-// This example is taken from https://learnopengl.com/
-// https://learnopengl.com/code_viewer.php?code=getting-started/hellowindow2
-// The code originally used GLEW, I replaced it with Glad
-
-// Compile:
-// g++ example/c++/hellowindow2.cpp -Ibuild/include build/src/glad.c -lglfw -ldl
+#include <iostream>
 
 
 // Function prototypes
@@ -21,13 +10,28 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
     std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-    // Init GLFW
+    
+    // Init and config GLFW
     glfwInit();
-    // Set all the required options for GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -52,6 +56,121 @@ int main()
         return -1;
     }
 
+
+    int success;
+    char infolog[512];
+
+    // vertex shader
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog << std::endl;
+    }
+
+
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infolog << std::endl;
+    }
+
+    
+    // link shaders
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infolog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infolog << std::endl;
+    }
+
+    // dispose shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    // set up vertex data (and buffers) and configure vertex attr - triangle
+    float vertices[] = {
+        // first triangle
+        -0.9f, -0.5f, 0.0f,  // left 
+        -0.0f, -0.5f, 0.0f,  // right
+        -0.45f, 0.5f, 0.0f,  // top 
+        // second triangle
+         0.0f, -0.5f, 0.0f,  // left
+         0.9f, -0.5f, 0.0f,  // right
+         0.45f, 0.5f, 0.0f   // top 
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // bind the vertex array object first, then bind and set vertex buffer, and then configure vertex attr
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    // set up vertex, index data and configure vertex attr - rectangle
+
+    /*
+    float evertices[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+    };
+
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAO);
+    // 2. copy our vertices array in a vertex buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(evertices), evertices, GL_STATIC_DRAW);
+    // 3. copy our index array in a element buffer for OpenGL to use
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 4. then set the vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    */
+
+
+    // wireframe polygons
+    glPointSize(18);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+
+
     // Define the viewport dimensions
     glViewport(0, 0, WIDTH, HEIGHT);
 
@@ -66,9 +185,26 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // draw triangle - vbo
+        // (because the bindings won't change, we could just set them once before the loop)
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // draw rectangle - ebo
+        //glUseProgram(shaderProgram);
+        //glBindVertexArray(VAO);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
+
         // Swap the screen buffers
         glfwSwapBuffers(window);
     }
+
+    // de-allocate all resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // Terminates GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
