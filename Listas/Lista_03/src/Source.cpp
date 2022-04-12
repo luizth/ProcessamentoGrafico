c13485ab810ebc3113c21b1b606ab7c6539cf25a
Luiz Thomasini <luizthomasini@gmail.com>
2022-04-12 07:17:01 -0300

    Hello World Lista_03

---
+++ Listas/Lista_03/src/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <cmath>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+int main()
+{
+	std::cout << "Hello World";
+}
